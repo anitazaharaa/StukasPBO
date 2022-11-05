b171c81df6edc677aad34fc214a358fa1934d2a7
Anita Zahara <101859284+anitazaharaa@users.noreply.github.com>
2022-11-05 19:39:31 +0700

    Create Pembayaran.java

---
+++ Pembayaran.java
@@ -0,0 +1,15 @@
+public class Pembayaran {
+    public int id_pengunjung;
+    public String nama;
+
+    public static void pembayaran(){
+        System.out.println("Anda sudah membayar Lunas");
+    }
+
+    public void DataPengunjung(){
+        System.out.println("Nama       : Anita");
+        System.out.println("Alamat     : Bandung");
+        System.out.println("No Hp      : 087897654321");
+        System.out.println("\n");
+    }
+}
