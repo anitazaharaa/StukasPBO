c1b3dcdd0de66665657d3c8de7539f66dc9890a2
Anita Zahara <101859284+anitazaharaa@users.noreply.github.com>
2022-11-05 19:40:25 +0700

    Create Informasi.java

---
+++ Informasi.java
@@ -0,0 +1,18 @@
+public class Informasi {
+    public String informasi;
+
+    public String getInformasi() {
+        return informasi;
+    }
+
+    public void setMakanan(String informasi) {
+        this.informasi = informasi;
+    }
+
+    public void DataInformasi(){
+        System.out.println("Rp. 85.000/orang dewasa untuk weekend");
+        System.out.println("Rp. 70.000/orang dewasa untuk weekdays");
+        System.out.println("Rp. 65.000/orang anak kecil untuk weekend");
+        System.out.println("Rp. 60.000/orang anak kecil untuk weekdays");
+    }
+}
